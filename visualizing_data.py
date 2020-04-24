669bdfc5ebb7dfcfff646c07b6663ae1bea564ac
neep305@gmail.com <neep9669>
2020-04-25 08:11:46 +0900

    add visualization

---
+++ visualizing_data.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+avocados = pd.read_pickle('./dataset/avoplotto.pkl')
+
+print(avocados.head())
+
+# Get the total number of avocados sold of each size
+nb_sold_by_size = avocados.groupby("size")["nb_sold"].sum()
+
+# Create a bar plot of the number of avocados sold by size
+nb_sold_by_size.plot(kind="bar")
+
+# Show the plot
+plt.show()
+
+# Get the total number of avocados sold on each date
+nb_sold_by_date = avocados.groupby("date")["nb_sold"].sum()
+
+# Create a line plot of the number of avocados sold by date
+nb_sold_by_date.plot(kind="line", x="date")
+
+# Show the plot
+plt.show()
+
+# Scatter plot of nb_sold vs avg_price with title
+avocados.plot(kind="scatter", x="nb_sold", y="avg_price", title="Number of avocados sold vs. average price")
+
+# Show the plot
+plt.show()
