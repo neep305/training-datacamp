0969d834a60f192e50289a00a668cc4423d1576d
neep305@gmail.com <neep9669>
2020-04-25 08:12:34 +0900

    add hist

--- visualizing_data.py
+++ visualizing_data.py
@@ -28,3 +28,15 @@ avocados.plot(kind="scatter", x="nb_sold", y="avg_price", title="Number of avoca
 
 # Show the plot
 plt.show()
+
+# Histogram of conventional avg_price
+avocados[avocados["type"] == "conventional"]["avg_price"].hist()
+
+# Histogram of organic avg_price
+avocados[avocados["type"] == "organic"]["avg_price"].hist()
+
+# Add a legend
+plt.legend(["conventional", "organic"])
+
+# Show the plot
+plt.show()
